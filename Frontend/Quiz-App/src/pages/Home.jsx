4979de00d5e2d1b826955d8e053c6bbf5a630f19
khushimehra <akmkhushimehra02@gmail.com>
2025-02-08 10:53:24 +0530

    set routes using react router dom

--- Frontend/Quiz-App/src/pages/Home.jsx
+++ Frontend/Quiz-App/src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+// src/pages/Home.tsx
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <header className="component">
+        <h1 className="heading">Challenge Yourself with Fun & Engaging Quizzes!</h1>
+        <div className="box">
+          <Link to="/quizzes">
+            <button className="btn">
+              Start a Quiz
+            </button>
+          </Link>
+          <Link to="/create-quiz">
+            <button className="btn">
+              Create a Quiz
+            </button>
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
