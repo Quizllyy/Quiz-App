37f5137328115383e98c360d9c8b8328e7c59fe8
Kirti Choudhary <kirtimmd2@gmail.com>
2025-03-14 11:25:58 +0530

    Name change

--- Frontend/Quiz-App/src/components/Navbar.jsx
+++ Frontend/Quiz-App/src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Stack, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <AppBar position="sticky" className="navbar">
       <Toolbar className="navComponent">
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} className="logo">
-          Quiz App
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+          className="logo">
+          Quizly
         </Typography>
         <Stack direction="row" spacing={2} className="navItems">
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/about">About Us</Button>
-          <Button color="inherit" component={Link} to="/contact">Contact Us</Button>
-          <Button color="inherit" component={Link} to="/signin">Sign In</Button>
-          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/about">
+            About Us
+          </Button>
+          <Button color="inherit" component={Link} to="/contact">
+            Contact Us
+          </Button>
+          <Button color="inherit" component={Link} to="/signin">
+            Sign In
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
         </Stack>
       </Toolbar>
     </AppBar>
